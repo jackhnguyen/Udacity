f66d246f3b5f1c241cdef9746e3103f8e997b674
jackhnguyen <jack.h.n.97@gmail.com>
2023-01-01 18:34:30 -0800

    first commit

---
+++ src/Contact.java
@@ -0,0 +1,10 @@
+class Contact{
+    private String friendName;
+    private String friendEmail;
+    private String friendPhoneNumber;
+    Contact(String name, String email, String phoneNumber){
+        friendName = name;
+        friendEmail = email
+        friendPhoneNumber = phoneNumber;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String [] args){
+        ContactManager CM = new ContactManager();
+        ContactManager.addContact(Contact Jackson = new Contact("jackson", "pp@gmail.com", "6572578078");)
+        ContactManager.addContact(Contact chiTam = new Contact("Tamle Nguyen", "yolo@gmail.com", "7147250440");)
+        ContactManager.searchContact("jackson");
+     }
+}
